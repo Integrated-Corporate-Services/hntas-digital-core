7860c7de902a464c0bd099e7502e7753834b6d23
SManokaran90 <sivasankar.manokaran@energysecurity.gov.uk>
2025-08-22 12:37:06 +0100

    Feature/dbh 121 dhb 184 invite new contributor changes (#38) (#39)
    * invitation changes
    
    * 1) Add new contributor changes
    2) Managed user changes
    3) Database restructure changes

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Models/InvitedUserResponse.cs
@@ -0,0 +1,32 @@
+﻿using HNTAS.Core.Api.Helpers;
+
+namespace HNTAS.Core.Api.Models
+{
+    public class InvitedUserResponse
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string? FullName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(FirstName) && string.IsNullOrWhiteSpace(LastName))
+                    return null;
+
+                var formattedFirst = StringFormatter.ToTitleCaseSingleWord(FirstName ?? "");
+                var formattedLast = StringFormatter.ToTitleCaseSingleWord(LastName ?? "");
+
+                return $"{formattedFirst} {formattedLast}".Trim();
+            }
+        }
+
+        public List<string> Roles { get; set; }
+        public string Status { get; set; }
+        public DateTime InvitedAt { get; set; }
+        public DateTime? AcceptedAt { get; set; }
+        public DateTime? RejectedAt { get; set; }
+    }
+}

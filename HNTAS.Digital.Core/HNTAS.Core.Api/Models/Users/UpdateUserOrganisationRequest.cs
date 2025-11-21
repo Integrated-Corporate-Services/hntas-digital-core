1618cc5d4464f4d3aed8c318607d7923dfb76952
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-11-21 13:09:19 +0000

    Fix for issue 358 and 360

--- HNTAS.Digital.Core/HNTAS.Core.Api/Models/Users/UpdateUserOrganisationRequest.cs
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Models/Users/UpdateUserOrganisationRequest.cs
@@ -3,30 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HNTAS.Core.Api.Models.Users
 {
-    public class UpdateUserOrganisationRequest
+    public class UpdateUserOrganisationRequest : UpdateUserDetailsRequest
     {
-        [Required(ErrorMessage = "First Name is required.")]
-        public string FirstName { get; set; } = null!;
-
-        [Required(ErrorMessage = "Last Name is required.")]
-        public string LastName { get; set; } = null!;
-
-        [Required(ErrorMessage = "Preferred Contact Type is required.")]
-        public PreferredContactType PreferredContactType { get; set; }
-
-        public string? LandlineNumber { get; set; }
-
-        public string? ContactNumberExtension { get; set; }
-
-        public string? MobileNumber { get; set; }
-
-        [Required(ErrorMessage = "Job Title is required.")]
-        public string JobTitle { get; set; } = null!;
-
-        [Required(ErrorMessage = "At least one role is required.")]
-        public UserRole Role { get; set; }
-
         [Required(ErrorMessage = "Organisation details are required.")]
         public OrganisationRequest Organisation { get; set; } = new OrganisationRequest();
+
+        [Required(ErrorMessage = "User role is required.")]
+        public new UserRole Role { get; set; }
     }
 }

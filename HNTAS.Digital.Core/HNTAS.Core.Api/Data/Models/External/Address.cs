d73d8f64d78f7f0d75f9b0012d0f2f40b38f7bb4
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-01-26 12:49:50 +0000

    Updated external api endpoint with added fields

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Data/Models/External/Address.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace HNTAS.Core.Api.Data.Models.External
+{
+    public class Address
+    {
+        [BsonElement("addressLine1")]
+        public string AddressLine1 { get; set; }
+
+        [BsonElement("addressLine2")]
+        public string? AddressLine2 { get; set; } // Added this
+
+        [BsonElement("town")]
+        public string? Town { get; set; }
+
+        [BsonElement("county")]
+        public string? County { get; set; } // Added this
+
+        [BsonElement("postcode")] // Ensure casing matches your DB ('postcode')
+        public string Postcode { get; set; }
+
+        [BsonElement("country")]
+        public string? Country { get; set; }
+    }
+}

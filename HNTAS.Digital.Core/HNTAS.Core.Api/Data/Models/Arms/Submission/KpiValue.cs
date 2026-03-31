748520e36656850fd692526291a05e3e1c033835
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-03-31 17:13:48 +0100

    Added ARMS Submission api post method

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Data/Models/Arms/Submission/KpiValue.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace HNTAS.Core.Api.Data.Models.Arms.Submission
+{
+    [JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
+    public class KpiValue
+    {
+        [JsonPropertyName("value")]
+        [BsonElement("value")]
+        public double Value { get; set; }
+
+        [JsonPropertyName("is_kpi_imputed")]
+        [BsonElement("isKpiImputed")]
+        public bool IsKpiImputed { get; set; } = false;
+
+        [JsonPropertyName("kpi_imputation_details")]
+        [BsonElement("kpiImputationDetails")]
+        public string? KpiImputationDetails { get; set; }
+    }
+}

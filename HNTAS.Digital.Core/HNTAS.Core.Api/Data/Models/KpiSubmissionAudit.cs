ec5e04434d36beaab1b786759ef9d539df6f4362
SManokaran90 <sivasankar.manokaran@energysecurity.gov.uk>
2026-04-24 17:26:22 +0100

    Added Audit logs for KpiSubmission (#188)

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Data/Models/KpiSubmissionAudit.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HNTAS.Core.Api.Data.Models
+{
+    public class KpiSubmissionAudit
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("submissionId")]
+        public string SubmissionId { get; set; }
+
+        [BsonElement("networkId")]
+        public string NetworkId { get; set; }
+
+
+        [BsonElement("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [BsonElement("sourceSystem")]
+        public string SourceSystem { get; set; }
+
+        [BsonElement("periodStart")]
+        public string PeriodStart { get; set; }
+
+        [BsonElement("changes")]
+        public List<KpiDeltaAudit> Changes { get; set; } = new();
+    }
+}

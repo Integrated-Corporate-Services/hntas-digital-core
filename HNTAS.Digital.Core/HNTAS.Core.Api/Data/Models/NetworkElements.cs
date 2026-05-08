ccad69c19c99dcc599bc376f4a7e3d566bdfd19d
Kuldeep-es <Kuldeep.Saini@energysecurity.gov.uk>
2026-05-08 15:46:53 +0100

    network elements v10 - wip

--- HNTAS.Digital.Core/HNTAS.Core.Api/Data/Models/NetworkElements.cs
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Data/Models/NetworkElements.cs
@@ -15,20 +15,6 @@ namespace HNTAS.Core.Api.Data.Models
         public NetworkDetailsStatus ElementSoaStatus { get; set; }
 
         [BsonElement("elements")]
-        public List<Element> Elements { get; set; } = [];
-        [BsonElement("distributionNetworksAndSubstations")]
-        public DistributionNetworksAndSubstations? DistributionNetworksAndSubstations { get; set; }
-    }
-
-    public class DistributionNetworksAndSubstations
-    {
-        [BsonElement("isAnyDistrictSubstations")]
-        public bool? IsAnyDistrictSubstations { get; set; }
-        [BsonElement("numberOfSubstations")]
-        [BsonRepresentation(BsonType.Int32)]
-        public int? NumberOfSubstations { get; set; }
-        [BsonElement("numberOfDistributionNetworks")]
-        [BsonRepresentation(BsonType.Int32)]
-        public int? NumberOfDistributionNetworks { get; set; }
-    }
+        public List<Element> Elements { get; set; } = [];        
+    }    
 }

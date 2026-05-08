5aed93e475cfc74eec6ee84f5d8da09c47c40b96
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-05-08 14:54:17 +0100

    arms api response changes

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Models/KpiSubmissionApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HNTAS.Core.Api.Models
+{
+    public class KpiSubmissionApiErrorResponse
+    {
+        public string Title { get; set; } = "Validation Failed";
+        public int Status { get; set; }
+        public string Detail { get; set; }
+        public List<KpiSubmissionApiError> Errors { get; set; } = new();
+    }
+}

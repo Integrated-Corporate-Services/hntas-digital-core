d6c173b37882619cee510b1c33db990298d20c2b
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-07-01 22:24:10 +0100

    Added Feedback service

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Models/CreateFeedbackRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace HNTAS.Core.Api.Models
+{
+    public class CreateFeedbackRequest
+    {
+        public string? SatisfactionLevel { get; set; }
+        public string? FeedbackText { get; set; }
+    }
+}

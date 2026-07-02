7bcd992de8b485f3f02bffaceadbb5cc9ec9b7d7
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-07-02 16:26:37 +0100

    Added Feedback service (#287)

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Controllers/FeedbackController.cs
@@ -0,0 +1,28 @@
+﻿using HNTAS.Core.Api.Models;
+using HNTAS.Core.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/feedback")]
+
+    public class FeedbackController : ControllerBase
+    {
+
+        private readonly IFeedbackService _service;
+
+        public FeedbackController(IFeedbackService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create(CreateFeedbackRequest request)
+        {
+            await _service.CreateAsync(request);
+            return Ok();
+        }
+    }
+}
---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Interfaces/IFeedbackService.cs
@@ -0,0 +1,9 @@
+﻿using HNTAS.Core.Api.Models;
+
+namespace HNTAS.Core.Api.Interfaces
+{
+    public interface IFeedbackService
+    {
+        Task CreateAsync(CreateFeedbackRequest request);
+    }
+}

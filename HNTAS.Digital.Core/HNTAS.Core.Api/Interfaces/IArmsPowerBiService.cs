de31af0a11b0e195e095e5e8b38653ae34cf074a
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-07-07 14:13:24 +0100

    Added Powerbi enpoint with unit testing

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Interfaces/IArmsPowerBiService.cs
@@ -0,0 +1,9 @@
+﻿using HNTAS.Core.Api.Models.Arms.PowerBi;
+
+namespace HNTAS.Core.Api.Interfaces
+{
+    public interface IArmsPowerBiService
+    {
+        Task<List<ArmsPowerBiReportResult>> GetPowerBiDataAsync();
+    }
+}

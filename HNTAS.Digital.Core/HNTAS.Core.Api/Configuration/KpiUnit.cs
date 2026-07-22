e56fda940092bd0a09c4e180e031d1515492f485
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-07-22 10:41:00 +0100

    Added units.json file with kpi and unit mapping and created a service to read the file

---
+++ HNTAS.Digital.Core/HNTAS.Core.Api/Configuration/KpiUnit.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HNTAS.Core.Api.Configuration
+{
+    [ExcludeFromCodeCoverage]
+    public class KpiUnit
+    {
+        public string KpiId { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+    }
+}
